c153ce809d2b3cf95c2b81b8105a09146f0ef264
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-12-11 16:38:53 -0300

    feat: Finalizes docker image

--- image/commands/find-unignored-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR=${1:-"."}
-FILTER=${2:-".*"}
-
-find "${DIR}" -path ./.git -prune -o -type f -regex "${FILTER}" -print | git check-ignore --verbose --non-matching --stdin | grep "::" | awk '{print $2}'
